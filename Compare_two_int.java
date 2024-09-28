ea658b7892958cab70830e8cc4b153295bbcdf06
Vignesh061 <vigneshvicky3611@gmail.com>
2024-09-28 22:35:20 +0530

    <-Created

---
+++ Compare_two_int.java
@@ -0,0 +1,19 @@
+//Q5).Write a Java program that compares two integers and prints whether they are equal or not. 
+//The program should use an if-else statement to check the equality of the two integers and display the appropriate message.
+class Compare_two_int{
+    public static void main(String arg[])
+    {
+        int num1=40;
+        int num2=50;
+         if(num1==num2)
+         {
+            System.out.print("num1 and num2 are equal ");
+         }
+         else
+         {
+            System.out.print("num1 and num2 are not equal");
+         }
+        
+
+    }
+}
---
+++ User_data.java
@@ -0,0 +1,18 @@
+//Write a Java program that prompts the user to enter their name and age.
+// The program should then display the entered name and age in the following format:
+import java.util.Scanner;
+class User_data{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name=sc.nextLine();
+        System.out.print("Enter your age:";)
+        int age=sc.nextInt();
+         System.out.println("Your name is :"+name);
+         System.out.println("Your age is :"+age);
+
+
+
+    } 
+}
