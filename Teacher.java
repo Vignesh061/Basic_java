1296fba30064aa120a42f87afc1cf3770c033f76
Vignesh061 <vigneshvicky3611@gmail.com>
2024-10-16 23:04:23 +0530

    Created

---
+++ Teacher.java
@@ -0,0 +1,18 @@
+class Teacher{
+    String name;
+    Teacher()
+    {
+        System.out.println("Hello");
+
+    }
+    void myname( String name)
+    {
+       this.name=na
+    }
+    public static void main(String arg[])
+    {
+        Teacher  t1 =new Teacher();
+        t1.myname("world");
+        System.out.println(t1.name);
+    }
+}
